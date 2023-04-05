10c012dad9c7337be4dd74dfae12f8b1458078f6
bittu1040 <32195733+bittu1040@users.noreply.github.com>
2023-04-05 09:43:17 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ngClass-and-ngStyle-example-in-angular
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ngclass-ngstyle-4uwqyf)
---
+++ src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+ 
+ isButton = true;
+ isPrimary = true;
+
+  canSubmit = false;
+
+ toggleButton() {
+   this.isPrimary = !this.isPrimary;
+ }
+
+ step1=true
+ flag= true;
+ toggleButtonForngStyle(){
+   this.flag= !this.flag
+ }
+
+ toggleButtonForngClass(){
+   this.step1=!this.step1
+ }
+}
